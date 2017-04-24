31c945efa0a35c83d2213b3bbe7026b04345c797
Shrawan Lakhe <shrawanlakhey@gmail.com>
2017-04-24 21:52:03 +0545

    handlebars routing...

--- routes/index.js
+++ routes/index.js
@@ -12,4 +12,50 @@ module.exports = function (app) {
             title: "Mount Everest"
         });
     });
+
+    app.get('/contact-us', function(req, res , next) {
+        res.render("pages/contact", {
+            homepageContent: "my country is Nepal",
+            title: "Mount Everest"
+        });
+    });
+
+    app.get('/about-us', function(req, res , next) {
+        res.render("pages/about", {
+            homepageContent: "my country is Nepal",
+            title: "Mount Everest"
+        });
+    });
+
+    app.get('/gallery', function(req, res , next) {
+        res.render("pages/gallery", {
+            homepageContent: "my country is Nepal",
+            title: "Mount Everest"
+        });
+    });
+
+    app.get('/blog-fullwidth', function(req, res , next) {
+        res.render("pages/blog-fullwidth", {
+            homepageContent: "my country is Nepal",
+            title: "Mount Everest"
+        });
+    });
+
+    app.get('/blog-left-sidebar', function(req, res , next) {
+        res.render("pages/blog-left-sidebar", {
+            homepageContent: "my country is Nepal",
+            title: "Mount Everest"
+        });
+    });
+
+    app.get('/blog-right-sidebar', function(req, res , next) {
+        res.render("pages/blog-right-sidebar", {
+            homepageContent: "my country is Nepal",
+            title: "Mount Everest"
+        });
+    });
+
+    app.get('*', function(req, res , next) {
+        res.render("pages/404");
+    });
 };
